73fe8d5f2ec2ff61853d2741447a5766b84ac08f
miakuricATU <X00218710@mytudublin.ie>
2025-05-16 18:05:14 +0100

    fixed logs feature

--- BPCalculator/Program.cs
+++ BPCalculator/Program.cs
@@ -15,6 +15,23 @@ namespace BPCalculator
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up the application");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
 
             CreateHostBuilder(args).Build().Run();
 
@@ -22,6 +39,7 @@ namespace BPCalculator
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+             .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
