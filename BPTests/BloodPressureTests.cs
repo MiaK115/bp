68ab736f298e8cc8db208a98e07e5ebae94c1833
miakuricATU <X00218710@mytudublin.ie>
2025-05-16 19:16:37 +0100

    expanding on test cases

--- BPTests/BloodPressureTests.cs
+++ BPTests/BloodPressureTests.cs
@@ -6,10 +6,15 @@ namespace BPTests
     public class BloodPressureTests
     {
         [Theory]
-        [InlineData(150, 95, BPCategory.High)]
+        [InlineData(145, 95, BPCategory.High)]
         [InlineData(85, 55, BPCategory.Low)]
-        [InlineData(130, 85, BPCategory.PreHigh)]
+        [InlineData(130, 70, BPCategory.PreHigh)]
         [InlineData(110, 70, BPCategory.Ideal)]
+        [InlineData(125, 85, BPCategory.PreHigh)]
+        [InlineData(95, 50, BPCategory.Low)]
+        [InlineData(140, 80, BPCategory.High)]
+        [InlineData(90, 60, BPCategory.Ideal)]
+        [InlineData(121, 89, BPCategory.PreHigh)]
         public void Category_ReturnsExpectedCategory(int systolic, int diastolic, BPCategory expected)
         {
             // Arrange
