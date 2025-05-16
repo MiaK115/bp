637e960db262398465d4c7ff9f842d3ac4b13516
miakuricATU <X00218710@mytudublin.ie>
2025-05-16 18:43:22 +0100

    added new test

--- BPTests/BloodPressureTests.cs
+++ BPTests/BloodPressureTests.cs
@@ -1,12 +1,29 @@
-﻿using BPCalculator;
+﻿using Xunit;
+using BPCalculator;
 
-namespace BPTests;
-
-public class BloodPressureTests
+namespace BPTests
 {
-    [Fact]
-    public void Test1()
+    public class BloodPressureTests
     {
+        [Theory]
+        [InlineData(150, 95, BPCategory.High)]
+        [InlineData(85, 55, BPCategory.Low)]
+        [InlineData(130, 85, BPCategory.PreHigh)]
+        [InlineData(110, 70, BPCategory.Ideal)]
+        public void Category_ReturnsExpectedCategory(int systolic, int diastolic, BPCategory expected)
+        {
+            // Arrange
+            var bp = new BloodPressure
+            {
+                Systolic = systolic,
+                Diastolic = diastolic
+            };
 
+            // Act
+            var result = bp.Category;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
