aff35b53a7a9a9cd2edfc9f7a70fabb42108aba6
miakuricATU <X00218710@mytudublin.ie>
2025-05-17 11:09:08 +0100

    changes to configuration settings

--- BPCalculator/Program.cs
+++ BPCalculator/Program.cs
@@ -16,12 +16,16 @@ namespace BPCalculator
         public static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
                 .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
                 .CreateLogger();
 
             try
             {
                 Log.Information("Starting up the application");
+
+                Console.WriteLine("Now listening on: http://localhost:5000");
+
                 CreateHostBuilder(args).Build().Run();
             }
             catch (Exception ex)
@@ -32,8 +36,6 @@ namespace BPCalculator
             {
                 Log.CloseAndFlush();
             }
-
-            CreateHostBuilder(args).Build().Run();
 
         }
 
