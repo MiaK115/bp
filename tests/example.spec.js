661e3de8970d7b83e6eb54d31c2d412321c71e25
miakuricATU <X00218710@mytudublin.ie>
2025-05-27 15:06:50 +0100

    adding env-based BASE_URL

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,17 @@
-// tests/homepage.spec.js
-const { test, expect } = require('@playwright/test');
+// // tests/homepage.spec.js
+// const { test, expect } = require('@playwright/test');
+
+// test('homepage loads and shows welcome message', async ({ page }) => {
+//   await page.goto('http://localhost:5000');
+//   await expect(page).toHaveTitle(/BPCalculator/i);
+// });
+
+
+import { test, expect } from '@playwright/test';
+
+const BASE_URL = process.env.BASE_URL || 'http://localhost:5000';
 
 test('homepage loads and shows welcome message', async ({ page }) => {
-  await page.goto('http://localhost:5000');
+  await page.goto(BASE_URL);
   await expect(page).toHaveTitle(/BPCalculator/i);
 });
