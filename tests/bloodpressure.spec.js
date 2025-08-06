ee88cc3c65e393b65e9120d4b81364caa1238220
miakuricATU <X00218710@mytudublin.ie>
2025-08-06 15:22:48 +0100

    Update bloodpressure.spec.js

--- tests/bloodpressure.spec.js
+++ tests/bloodpressure.spec.js
@@ -12,18 +12,33 @@
 // });
 
 
+// import { test, expect } from '@playwright/test';
+
+// // Use environment variable if available, fallback to local
+// const BASE_URL = process.env.BASE_URL || 'http://localhost:5000';
+
+// test('User can calculate blood pressure category', async ({ page }) => {
+//     await page.goto(BASE_URL);
+
+//     await page.fill('input[name="BP.Systolic"]', '135');
+//     await page.fill('input[name="BP.Diastolic"]', '85');
+//     await page.click('text=Submit');
+
+//     // Assert expected result is shown
+//     await expect(page.locator('text=Pre-High Blood Pressure')).toBeVisible();
+// });
+
+
 import { test, expect } from '@playwright/test';
 
-// Use environment variable if available, fallback to local
-const BASE_URL = process.env.BASE_URL || 'http://localhost:5000';
+const baseURL = process.env.BASE_URL || 'http://localhost:5000';
 
 test('User can calculate blood pressure category', async ({ page }) => {
-    await page.goto(BASE_URL);
+    await page.goto(baseURL);
 
     await page.fill('input[name="BP.Systolic"]', '135');
     await page.fill('input[name="BP.Diastolic"]', '85');
     await page.click('text=Submit');
 
-    // Assert expected result is shown
     await expect(page.locator('text=Pre-High Blood Pressure')).toBeVisible();
 });
